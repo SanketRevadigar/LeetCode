0ce2505154d0494cba9b22520c6b0d81b8d3bbf6
SanketRevadigar <55157853+SanketRevadigar@users.noreply.github.com>
2022-01-22 14:51:36 -0500

    Time: 149 ms (33.79%), Space: 17.8 MB (53.90%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,9 +2,10 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         count = {}                                      #hashmap for key values
         for words in strs:                              #sort each word and add as a key
-            #sortedWord = ' '.join(sorted(words))        #use join to add word as str,dont assign directly
-            if str(sorted(words)) not in count:
-                count[str(sorted(words))] = [words]
+            sortedWord = ' '.join(sorted(words))        #use join to add word as str,dont assign directly
+                                                        # or use type casting as str(sorted(words))
+            if sortedWord not in count:                 #if SortedWord not in count add it as key &      
+                count[sortedWord] = [words]             #add original word as its value
             else:
-                count[str(sorted(words))].append(words)
-        return count.values()
+                count[sortedWord].append(words)         #if already present add it as its value
+        return count.values()                           #return values

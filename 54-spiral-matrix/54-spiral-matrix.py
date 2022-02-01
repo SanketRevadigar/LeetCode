75be829b6cb532c860cde03574589b342141198a
SanketRevadigar <55157853+SanketRevadigar@users.noreply.github.com>
2022-01-31 19:20:03 -0500

    Time: 47 ms (30.97%), Space: 14 MB (95.08%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,25 +1,25 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         ROW,COL=len(matrix),len(matrix[0])
-        left,top=0,0                                            #
-        right,bottom=COL,ROW
+        left,top=0,0                                            #initiaise 4 ptrs at 4 corners
+        right,bottom=COL,ROW                    
         res=[]
-        while left < right and top < bottom:
-            for i in range(left,right):
+        while left < right and top < bottom:                    #if all ptrs in range
+            for i in range(left,right):                         # top row iterating and appending   
                 res.append(matrix[top][i])
-            top += 1
+            top += 1                                            #Increment row as its visited
             
-            for i in range(top,bottom):
+            for i in range(top,bottom):                         #right row iterating
                 res.append(matrix[i][right-1])
-            right -= 1
+            right -= 1                                          #right -1 as its visited
             
-            if not (left < right and top < bottom):
+            if not (left < right and top < bottom):             #check again if all 4ptrs in range
                 break
             
-            for i in range(right - 1,left - 1, -1):
+            for i in range(right - 1,left - 1, -1):             #bottom row, reverse iterating
                 res.append(matrix[bottom - 1][i])
-            bottom -= 1
-            for i in range(bottom - 1, top - 1, -1):
-                res.append(matrix[i][left])
+            bottom -= 1                                         #decremnet bottom as its visited
+            for i in range(bottom - 1, top - 1, -1):            #finally 1st column
+                res.append(matrix[i][left])                     #append and then shift left inwards
             left += 1
         return res

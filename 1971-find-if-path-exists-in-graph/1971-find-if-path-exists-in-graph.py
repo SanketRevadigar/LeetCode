78aa255e9cf29d3ccbc803e5e8e85d4c98de8f91
Sanket Revadigar <55157853+SanketRevadigar@users.noreply.github.com>
2022-02-24 19:18:26 -0500

    Time: 2106 ms (78.55%), Space: 106.3 MB (68.45%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,18 +1,18 @@
 import collections
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        neighbours=defaultdict(list)
-        for n1,n2 in edges:
+        neighbours = defaultdict(list)
+        for n1,n2 in edges:                         #create a hashmap for graph neighbours
             neighbours[n1].append(n2)
-            neighbours[n2].append(n1)
-        queue,visited=deque(),set()
+            neighbours[n2].append(n1)               #as its undirected add both pairs
+        queue,visited = deque(),set()               #queue for bfs and visited to track edges
         queue.append(source)
         visited.add(source)
         while queue:
-            node=queue.popleft()
-            if node==destination:
+            node = queue.popleft()
+            if node == destination:                 #if current element is dest, return True
                 return True
-            for adj in neighbours[node]:
+            for adj in neighbours[node]:            #check for its neighbour edges also
                 if adj not in visited:
                     queue.append(adj)
                     visited.add(adj)
